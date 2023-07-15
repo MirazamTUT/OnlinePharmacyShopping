a48f0105b17b9b793962ec785ab8f139cf0cd47c
Iminjonov <129150301+mrIminjonov@users.noreply.github.com>
2023-07-15 20:37:43 +0500

    Update IPharmacyRepository.cs
    3526782345432617834 marta kechirim soreman

--- OnlinePharmacyShopping/PharmacyShopping.DataAccess/Repository/IRepositories/IPharmacyRepository.cs
+++ OnlinePharmacyShopping/PharmacyShopping.DataAccess/Repository/IRepositories/IPharmacyRepository.cs
@@ -11,9 +11,13 @@ namespace PharmacyShopping.DataAccess.Repository.IRepositories
     public interface IPharmacyRepository
     {
         Task<int> AddPharmacyAsync(Pharmacy pharmacy);
+        
         Task<Pharmacy> GetPharmacyByIdAsync(int pharmacyId);
+        
         Task<List<Pharmacy>> GetAllPharmacyAsync();
+        
         Task<int> UpdatePharmacyAsync(Pharmacy pharmacy);
+        
         Task<int> DeletePharmacyAsync(Pharmacy pharmacy);
     }
 }

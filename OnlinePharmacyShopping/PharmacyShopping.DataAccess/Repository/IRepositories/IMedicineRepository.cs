af55c07e2b91d1730c462f3442b82724a321a201
unknown <isroilov1922@gmail.com>
2023-07-15 12:48:10 +0500

    Added Medicine Repository

---
+++ OnlinePharmacyShopping/PharmacyShopping.DataAccess/Repository/IRepositories/IMedicineRepository.cs
@@ -0,0 +1,17 @@
+﻿using PharmacyShopping.DataAccess.Models;
+
+namespace PharmacyShopping.DataAccess.Repository.IRepositories
+{
+    public interface IMedicineRepository
+    {
+        Task<int> AddMedicineAsync(Medicine medicine);
+
+        Task<Medicine> GetMedicineByIdAsync(int id);
+
+        Task<List<Medicine>> GetAllMedicinesAsync();
+
+        Task<int> UpdateMedicineAsync(Medicine medicine);
+
+        Task<int> DeleteMedicineAsync(Medicine medicine);
+    }
+}

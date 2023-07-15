0b93b7c34ae12d68fee5e665df2f3bb4e059baa0
unknown <isroilov1922@gmail.com>
2023-07-15 20:36:43 +0500

    fixed dto

--- OnlinePharmacyShopping/PharmacyShopping.BusinessLogic/DTO/RequestDTOs/MedicineRequestDTO.cs
+++ OnlinePharmacyShopping/PharmacyShopping.BusinessLogic/DTO/RequestDTOs/MedicineRequestDTO.cs
@@ -2,6 +2,16 @@
 {
     public class MedicineRequestDTO
     {
+        public int DataBaseId { get; set; }
+
         public string MedicineName { get; set; }
+
+        public string MedicineCategory { get; set; }
+
+        public string MedicineDescription { get; set; }
+
+        public double MedicinePrice { get; set; }
+
+        public double AmountOfMedecine { get; set; }
     }
 }

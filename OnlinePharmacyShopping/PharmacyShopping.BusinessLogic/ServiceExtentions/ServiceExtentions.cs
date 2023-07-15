07c5f39573ace7a831b85aa700c457db60834b06
MirazamTUT <mmiromonov111@gmail.com>
2023-07-15 13:02:32 +0500

    Added SalesRepository

---
+++ OnlinePharmacyShopping/PharmacyShopping.BusinessLogic/ServiceExtentions/ServiceExtentions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PharmacyShopping.DataAccess.Repository.IRepositories;
+using PharmacyShopping.DataAccess.Repository.Repositories;
+
+namespace PharmacyShopping.BusinessLogic.ServiceExtentions
+{
+    public static class ServiceExtentions
+    {
+        public static void RegisterServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<ISalesRepository, SalesRepository>();
+        }
+    }
+}
---
+++ OnlinePharmacyShopping/PharmacyShopping.DataAccess/Repository/IRepositories/ISalesRepository.cs
@@ -0,0 +1,17 @@
+﻿using PharmacyShopping.DataAccess.Models;
+
+namespace PharmacyShopping.DataAccess.Repository.IRepositories
+{
+    public interface ISalesRepository
+    {
+        Task<int> AddSales(Sales sales);
+
+        Task<int> UpdateSales(Sales sales);
+
+        Task<int> DeleteSales(Sales sales);
+
+        Task<List<Sales>> GetAllSales();
+
+        Task<Sales> GetSalesByCustomerId(int id);
+    }
+}

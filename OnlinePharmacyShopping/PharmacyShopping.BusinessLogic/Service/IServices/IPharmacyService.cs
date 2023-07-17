3dd0d17d09aed26a559791272e8a01990d368ed4
MirazamTUT <mmiromonov111@gmail.com>
2023-07-17 12:08:41 +0500

    Added Service

---
+++ OnlinePharmacyShopping/PharmacyShopping.BusinessLogic/Service/IServices/IPharmacyService.cs
@@ -0,0 +1,19 @@
+﻿using PharmacyShopping.BusinessLogic.DTO.RequestDTOs;
+using PharmacyShopping.BusinessLogic.DTO.ResponseDTOs;
+using PharmacyShopping.DataAccess.Models;
+
+namespace PharmacyShopping.BusinessLogic.Service.IServices
+{
+    public interface IPharmacyService
+    {
+        Task<int> AddPharmacyAsync(PharmacyRequestDTO pharmacyRequestDto);
+
+        Task<PharmacyResponseDTO> GetPharmacyByIdAsync(int id);
+
+        Task<List<PharmacyResponseDTO>> GetAllPharmacyAsync();
+
+        Task<int> UpdatePharmacyAsync(PharmacyRequestDTO pharmacyRequestDTO);
+
+        Task<int> DeletePharmacyAsync(int id);
+    }
+}
---
+++ OnlinePharmacyShopping/PharmacyShopping.BusinessLogic/Service/Services/PharmacyService.cs
@@ -0,0 +1,42 @@
+﻿using PharmacyShopping.BusinessLogic.DTO.RequestDTOs;
+using PharmacyShopping.BusinessLogic.DTO.ResponseDTOs;
+using PharmacyShopping.BusinessLogic.Service.IServices;
+using PharmacyShopping.DataAccess.Repository.IRepositories;
+
+namespace PharmacyShopping.BusinessLogic.Service.Services
+{
+    public class PharmacyService : IPharmacyService
+    {
+        private readonly IPharmacyRepository _repository;
+
+        public PharmacyService(IPharmacyRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<int> AddPharmacyAsync(PharmacyRequestDTO pharmacyRequestDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeletePharmacyAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<PharmacyResponseDTO>> GetAllPharmacyAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PharmacyResponseDTO> GetPharmacyByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpdatePharmacyAsync(PharmacyRequestDTO pharmacyRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

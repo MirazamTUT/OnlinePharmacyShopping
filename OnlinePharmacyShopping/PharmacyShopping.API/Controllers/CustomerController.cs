a9cada1b96d6f856ab3c964366dfc5830604e4d9
MirazamTUT <mmiromonov111@gmail.com>
2023-07-21 16:01:42 +0500

    added CustomerController

---
+++ OnlinePharmacyShopping/PharmacyShopping.API/Controllers/CustomerController.cs
@@ -0,0 +1,122 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PharmacyShopping.BusinessLogic.DTO.RequestDTOs;
+using PharmacyShopping.BusinessLogic.DTO.ResponseDTOs;
+using PharmacyShopping.BusinessLogic.Service.IServices;
+
+namespace PharmacyShopping.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ICustomerService _customerService;
+
+        public CustomerController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<int>> AddCustomerAsync(CustomerRequestDTO customerRequestDTO)
+        {
+            try
+            {
+                return await _customerService.AddCustomerAsync(customerRequestDTO);
+            }
+            catch (AutoMapperMappingException ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+            catch (DbUpdateException ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, ex.Message);
+            }
+        }
+
+        [HttpGet("Id")]
+        public async Task<ActionResult<CustomerResponseDTO>> GetCustomerByIdAsync(int id)
+        {
+            try
+            {
+                return await _customerService.GetCustomerByIdAsync(id);
+            }
+            catch (AutoMapperMappingException ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+            catch (InvalidOperationException ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpGet("All")]
+        public async Task<ActionResult<List<CustomerResponseDTO>>> GetAllCustomersAsync()
+        {
+            try
+            {
+                return await _customerService.GetAllCustomersAsync();
+            }
+            catch (AutoMapperMappingException ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+            catch (InvalidOperationException ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<int>> UpdateCustomerAsync(CustomerRequestDTO customerRequestDTO, int id)
+        {
+            try
+            {
+                return await _customerService.UpdateCustomerAsync(customerRequestDTO, id);
+            }
+            catch (AutoMapperMappingException ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+            catch (DbUpdateException ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<int>> DeleteCustomerAsync(int id)
+        {
+            try
+            {
+                return await _customerService.DeleteCustomerAsync(id);
+            }
+            catch (DbUpdateException ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, ex.Message);
+            }
+        }
+    }
+}

51e1a045c0a5738f16e2c2db72059e7aeb2d7df5
unknown <isroilov1922@gmail.com>
2023-07-24 12:21:29 +0500

    Updated Validation

--- OnlinePharmacyShopping/PharmacyShopping.BusinessLogic/Service/IServices/IDataBaseService.cs
+++ OnlinePharmacyShopping/PharmacyShopping.BusinessLogic/Service/IServices/IDataBaseService.cs
@@ -1,15 +1,15 @@
-﻿using PharmacyShopping.BusinessLogic.DTO.ResponseDTOs;
-using PharmacyShopping.DataAccess.Models;
+﻿using PharmacyShopping.BusinessLogic.DTO.RequestDTOs;
+using PharmacyShopping.BusinessLogic.DTO.ResponseDTOs;
 
 namespace PharmacyShopping.BusinessLogic.Service.IServices
 {
     public interface IDataBaseService
     {
-        Task<int> AddDataBaseAsync(DataBase dataBase);
+        Task<int> AddDataBaseAsync(DataBaseRequestDTO dataBaseRequestDTO);
 
         Task<DataBaseResponseDTO> GetDataBaseAsync();
 
-        Task<int> UpdateDataBaseAsync(DataBase dataBase, int id);
+        Task<int> UpdateDataBaseAsync(DataBaseRequestDTO dataBaseRequestDTO, int id);
 
         Task<int> DeleteDataBaseAsync(int id);
     }

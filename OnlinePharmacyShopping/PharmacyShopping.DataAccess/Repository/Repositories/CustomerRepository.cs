402ee127a356d599824bc34296de16907822e037
MirazamTUT <mmiromonov111@gmail.com>
2024-02-10 14:13:01 +0500

    ChangedGetByFirstAndLastNamesMethod

--- OnlinePharmacyShopping/PharmacyShopping.DataAccess/Repository/Repositories/CustomerRepository.cs
+++ OnlinePharmacyShopping/PharmacyShopping.DataAccess/Repository/Repositories/CustomerRepository.cs
@@ -1,20 +1,25 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Dapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Npgsql;
 using PharmacyShopping.DataAccess.DbConnection;
 using PharmacyShopping.DataAccess.Models;
 using PharmacyShopping.DataAccess.Repository.IRepositories;
+using System.Data;
 
 namespace PharmacyShopping.DataAccess.Repository.Repositories
 {
     public class CustomerRepository : ICustomerRepository
     {
         private readonly PharmacyDbContext _pharmacyDbContext;
+        private readonly IConfiguration _configuration;
         private readonly ILogger<CustomerRepository> _logger;
 
-        public CustomerRepository(PharmacyDbContext pharmacyDbContext, ILogger<CustomerRepository> logger)
+        public CustomerRepository(PharmacyDbContext pharmacyDbContext, ILogger<CustomerRepository> logger, IConfiguration configuration)
         {
             _pharmacyDbContext = pharmacyDbContext;
+            _configuration = configuration;
             _logger = logger;
         }
 
@@ -93,14 +98,47 @@ namespace PharmacyShopping.DataAccess.Repository.Repositories
         {
             try
             {
-                _logger.LogInformation("nma diyishniham bilmiman.");
-                return await _pharmacyDbContext.Customers
-                    .Include(x => x.Purchases)
-                    .Include(x => x.Sales)
-                    .Include(x => x.Reports)
-                    .AsSplitQuery()
-                    .FirstOrDefaultAsync(x => x.CustomerFirstName == customerFirstName && x.CustomerLastName == customerLastName);
+                _logger.LogInformation("Attempting to retrieve customer information.");
 
+                string connectionString = _configuration.GetConnectionString("DefaultConnect");
+
+                using (var connection = new NpgsqlConnection(connectionString))
+                {
+                    await connection.OpenAsync();
+
+                    string query = "SELECT * FROM Customers WHERE CustomerFirstName = @CustomerFirstName AND CustomerLastName = @CustomerLastName LIMIT 1";
+
+                    using (var command = new NpgsqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("CustomerFirstName", customerFirstName);
+                        command.Parameters.AddWithValue("CustomerLastName", customerLastName);
+
+                        using (var reader = await command.ExecuteReaderAsync())
+                        {
+                            if (await reader.ReadAsync())
+                            {
+                                var customer = new Customer
+                                {
+                                    CustomerId = reader.GetInt32(reader.GetOrdinal("CustomerId")),
+                                    CustomerFirstName = reader.GetString(reader.GetOrdinal("CustomerFirstName")),
+                                    CustomerLastName = reader.GetString(reader.GetOrdinal("CustomerLastName")),
+                                };
+                                return customer;
+                            }
+                            else
+                            {
+                                throw new Exception("ishlamadi");
+                            }
+                        }
+                    }
+                }
+
+                /*_logger.LogInformation("nma diyishniham bilmiman.");
+                using (IDbConnection dbConnection =  (IDbConnection)_configuration.GetSection("ConnectionStrings:DefaultConnect"))
+                {
+                    string query = "SELECT TOP 1 * FROM Customers WHERE CustomerFirstName = @CustomerFirstName AND CustomerLastName = @CustomerLastName";
+                    return await dbConnection.QueryFirstOrDefaultAsync<Customer>(query, new { CustomerFirstName = customerFirstName, CustomerLastName = customerLastName });
+                }*/
             }
             catch (InvalidOperationException ex)
             {
